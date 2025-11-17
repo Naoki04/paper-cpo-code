34026aa80d721cebf24a767bb84276d50e2f52e0
Naoki04 <naoki.shitanda.phone@gmail.com>
2025-11-17 00:11:05 +0000

    Implement FrankaCabinet, FrankaCubeStack

--- isaacgymenvs/pbt/launcher/experiment_args.py
+++ isaacgymenvs/pbt/launcher/experiment_args.py
@@ -22,7 +22,7 @@ def add_experiment_args(parser):
     #     "Experiment module must be importable in your Python environment. It must define a global variable EXPERIMENT_DESCRIPTION (see existing experiment modules for examples).",
     # )
     
-    parser.add_argument("--env", default="allegro_kuka", choices=["ant","allegro_kuka", "allegro_kuka_two_arms","franka_cube_stack","franka_cube_push", "shadow_hand", "allegro_hand", "trifinger", "franka_cabinet", "humanoid", "cont_mountain_car", "anymal", "parkour"], type=str, help="Environment to run")
+    parser.add_argument("--env", default="allegro_kuka", choices=["ant","allegro_kuka", "allegro_kuka_two_arms","franka_cube_stack","franka_cube_push", "shadow_hand", "allegro_hand", "trifinger", "franka_cabinet", "humanoid", "cont_mountain_car", "anymal", "parkour", "franka_cube_stack", "franka_cabinet"], type=str, help="Environment to run")
     parser.add_argument("--task" , default="reorientation", type=str, help="Task to run")
     parser.add_argument("--sparse-reward", action='store_true', help="Whether to use sparse reward")
     parser.add_argument("--episode-length", default=None, type=int, help="Episode length")
@@ -94,7 +94,7 @@ def get_experiment_run_description(args):
         "humanoid" : "Humanoid",
         "cont_mountain_car" : "ContinuousMountainCar",
         "anymal" : "Anymal",
-        "parkour" : "Parkour"
+        "parkour" : "Parkour",
     }
 
     mutation_dict = {
@@ -110,7 +110,8 @@ def get_experiment_run_description(args):
         "humanoid" : "default_mutation",
         "cont_mountain_car" : "default_mutation",
         "anymal" : "default_mutation",
-        "parkour" : "default_mutation"
+        "parkour" : "default_mutation",
+        "franka_cube_stack": "default_mutation",
     }
 
     if args.minibatch_size is None:
